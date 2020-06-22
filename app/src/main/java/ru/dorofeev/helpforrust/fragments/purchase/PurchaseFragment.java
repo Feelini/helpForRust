f23e514ef9aeec25f60d65f6f5ce1b8b7cf407b8
user <user@example.com>
2020-06-22 16:16:44 +0300

    billing added

--- app/src/main/java/ru/dorofeev/helpforrust/fragments/purchase/PurchaseFragment.java
+++ app/src/main/java/ru/dorofeev/helpforrust/fragments/purchase/PurchaseFragment.java
@@ -1,19 +1,49 @@
 package ru.dorofeev.helpforrust.fragments.purchase;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import ru.dorofeev.helpforrust.R;
 
 public class PurchaseFragment extends Fragment {
 
     private static PurchaseFragment instance;
+    @BindView(R.id.adsOff)
+    Button adsOff;
+    @BindView(R.id.purchaseText)
+    TextView purchaseText;
+    private Unbinder unbinder;
+    private String mSkuId = "remove_ads";
+    private BillingClient billingClient;
+    private Map<String, SkuDetails> mSkuDetailsMap = new HashMap<>();
 
     public static PurchaseFragment getInstance() {
         if (instance == null) {
@@ -31,5 +61,85 @@ public class PurchaseFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+
+        initBilling();
+
+        adsOff.setOnClickListener(v -> launchBilling(mSkuId));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (unbinder != null){
+            unbinder.unbind();
+        }
+    }
+
+
+    private void initBilling() {
+        billingClient = BillingClient.newBuilder(getContext()).setListener((billingResult, list) -> {
+            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && list != null) {
+                payComplete();
+            }
+        }).build();
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    //below you can query information about products and purchase
+                    querySkuDetails(); //query for products
+                    List<Purchase> purchasesList = queryPurchases(); //query for purchases
+
+                    //if the purchase has already been made to give the goods
+                    for (int i = 0; i < purchasesList.size(); i++) {
+                        String purchaseId = purchasesList.get(i).getSku();
+                        if(TextUtils.equals(mSkuId, purchaseId)) {
+                            payComplete();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                //here when something went wrong, e.g. no internet connection
+            }
+        });
+    }
+
+    private void querySkuDetails() {
+        SkuDetailsParams.Builder skuDetailsParamsBuilder = SkuDetailsParams.newBuilder();
+        List<String> skuList = new ArrayList<>();
+        skuList.add(mSkuId);
+        skuDetailsParamsBuilder.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
+        billingClient.querySkuDetailsAsync(skuDetailsParamsBuilder.build(), new SkuDetailsResponseListener() {
+            @Override
+            public void onSkuDetailsResponse(@NonNull BillingResult billingResult, @Nullable List<SkuDetails> list) {
+                if (billingResult.getResponseCode() == 0) {
+                    for (SkuDetails skuDetails : list) {
+                        mSkuDetailsMap.put(skuDetails.getSku(), skuDetails);
+                    }
+                }
+            }
+        });
+    }
+
+    private List<Purchase> queryPurchases() {
+        Purchase.PurchasesResult purchasesResult = billingClient.queryPurchases(BillingClient.SkuType.INAPP);
+        return purchasesResult.getPurchasesList();
+    }
+
+    public void launchBilling(String skuId) {
+        BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                .setSkuDetails(mSkuDetailsMap.get(skuId))
+                .build();
+        billingClient.launchBillingFlow(getActivity(), billingFlowParams);
+    }
+
+    private void payComplete() {
+        purchaseText.setText(getString(R.string.thanks));
+        adsOff.setClickable(false);
+//        Toast.makeText(getContext(), "Pay complete", Toast.LENGTH_SHORT).show();
     }
 }
